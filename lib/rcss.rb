119ebe4706984deb70ab427dafc8ff50bc385fa8
Julius Parishy <juliusparishy@gmail.com>
2012-05-24 00:51:42 -0400

    Generate some basic changes.
    Added logic to generate some basic changes. Won't do anything to crazy
    yet, but will generate a bunch of simple animations as testing starts
    kicking in.

--- lib/rcss.rb
+++ lib/rcss.rb
@@ -6,6 +6,14 @@ class CSSBaseSerializable
     
   end
   
+  def at_strip(s)
+    if s.start_with? "@@"
+      s = s[2..s.length]
+    end
+    
+    s
+  end
+  
 end
 
 class CSSTransform < CSSBaseSerializable
@@ -37,29 +45,47 @@ class CSSAnimation < CSSBaseSerializable
     cached_variables = { }
     
     self.class.class_variables.each do |var|
-      cached_variables[var.to_s] = self.class.class_variable_get(var)
+      cached_variables[at_strip(var.to_s)] = self.class.class_variable_get(var)
     end
     
     cached_variables
   end
   
+  def generate_animation_for_variable(name, old_value, new_value)
+    "@-webkit-animation #{name}-anim {
+      from {
+        #{name}: #{old_value};
+      }
+      
+      to {
+        #{name}: #{new_value};
+      }
+    }"
+  end
+  
+  def generate_animation_for_variables(old_vars, new_vars)
+    anims = [ ]
+    old_vars.each do |name, var|
+      anims << generate_animation_for_variable(name, var, new_vars[name])
+    end
+    
+    puts anims
+  end
+  
   def simulate_animation(duration, delay, options, animationLambda)
     
     # Cache values
-    vars = cache_current_values
-    
-    #puts vars
-    
+    old_vars = cache_current_values
     animationLambda.call
+    new_vars = cache_current_values
     
-    # Calculate animations
+    # Generate the animations
+    generate_animation_for_variables old_vars, new_vars
   end
   
   def animation(duration, delay, options, animationLambda, completionLambda)
     
-    # Do the animations....
     simulate_animation duration, delay, options, animationLambda
-    
     completionLambda.call
   end
 end
@@ -67,15 +93,17 @@ end
 class MyAnimation < CSSAnimation
   
   def initialize
-    @@transform = CSSTransform.identity
+    @@width = 500
   end
   
   def do_animations
     animation 1, 0, nil, -> {
       # Some animations
+      @@width = 1000
     }, -> { 
       animation 1, 0, nil, -> {
         # Some more animations
+        @@width = 500
       }, -> {
         # Done!
       }
